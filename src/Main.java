0325392639542325af097d5892df9afa73e8634b
Kiraign <131566110+Kiraign@users.noreply.github.com>
2023-06-11 01:50:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int height = (int) 1.60;
+        double height = 1.60;
         int weight = 50;
         int index = service.calculate(height, weight);
         System.out.println(index);
